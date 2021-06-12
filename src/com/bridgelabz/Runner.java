a6d9244ec8f277ee61fa6e193f0b72e2996d09da
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-12 17:16:15 +0530

    Refactor:corrected the logic

--- src/com/bridgelabz/Runner.java
+++ src/com/bridgelabz/Runner.java
@@ -3,9 +3,9 @@ package com.bridgelabz;
 public class Runner {
 	public static void main(String[] args) {
 		LinkedList<Integer> numberList = new LinkedList<Integer>();
-		numberList.add(30);
-		numberList.add(56);
 		numberList.addAtStart(70);
+		numberList.addAtStart(30);
+		numberList.addAtStart(56);
 		numberList.show();
 	}
 }
