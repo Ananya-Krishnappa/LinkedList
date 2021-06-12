34f44f5dcc8d4e54ee355b2deab57a8eafc3ebf4
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-12 16:28:57 +0530

    Create linked list

---
+++ src/com/bridgelabz/Runner.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class Runner {
+	public static void main(String[] args) {
+		LinkedList<Integer> numberList = new LinkedList<Integer>();
+		numberList.add(56);
+		numberList.add(30);
+		numberList.add(70);
+		numberList.show();
+	}
+}
