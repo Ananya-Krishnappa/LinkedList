a500500a2645984f54acb21fcae72550cf46899f
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-12 17:52:51 +0530

    Added:Add at specified index

--- src/com/bridgelabz/Runner.java
+++ src/com/bridgelabz/Runner.java
@@ -4,8 +4,8 @@ public class Runner {
 	public static void main(String[] args) {
 		LinkedList<Integer> numberList = new LinkedList<Integer>();
 		numberList.add(56);
-		numberList.add(30);
 		numberList.add(70);
+		numberList.addAtIndex(30, 1);
 		numberList.show();
 	}
 }
