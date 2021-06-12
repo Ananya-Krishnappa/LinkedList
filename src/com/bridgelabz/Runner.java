ccb89c0292aef14c0e6f8633f98ee84592454e59
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-12 20:03:11 +0530

    Added:search by value

--- src/com/bridgelabz/Runner.java
+++ src/com/bridgelabz/Runner.java
@@ -6,11 +6,11 @@ public class Runner {
 		numberList.add(56);
 		numberList.add(30);
 		numberList.add(70);
-		System.out.println("Array before popLast");
+		System.out.println("Array elements");
 		numberList.show();
 		System.out.println();
-		numberList.popLast();
-		System.out.println("Array after popLast");
-		numberList.show();
+		int searchResult = numberList.searchByValue(90);
+		String result = (searchResult == -1) ? "value not found in the list" : "value found at index " + searchResult;
+		System.out.println(result);
 	}
 }
