2d24b9d2394402f6e5931d2cec126c796e174f9f
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-12 20:57:15 +0530

    Added:deleting at given index

--- src/com/bridgelabz/Runner.java
+++ src/com/bridgelabz/Runner.java
@@ -5,12 +5,14 @@ public class Runner {
 		LinkedList<Integer> numberList = new LinkedList<Integer>();
 		numberList.add(56);
 		numberList.add(30);
+		numberList.add(40);
 		numberList.add(70);
 		System.out.println("Array elements");
 		numberList.show();
 		System.out.println();
-		int searchResult = numberList.searchByValue(30);
-		numberList.addAtIndex(40, searchResult+1);
+		int searchResult = numberList.searchByValue(40);
+		numberList.popAtIndex(searchResult);
 		numberList.show();
+		System.out.println("The size of the list is " + numberList.size());
 	}
 }
